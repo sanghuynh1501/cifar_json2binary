e4f9286462d5d0a300d7f0c7f5c3bcddb0b28f19
sang huynh <sanghuynhnt95@gmail.com>
2020-02-13 14:55:49 +0700

    fix

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"time"
 )
 
 type Weight struct {
@@ -20,7 +19,6 @@ type Data struct {
 }
 
 func Load() Data {
-	// jsonFile, err := openuri.Open("http://localhost:3000/weights.json")
 	jsonFile, err := os.Open("weights.json")
 
 	if err != nil {
@@ -36,17 +34,9 @@ func Load() Data {
 	return data
 }
 
-func elapsed(what string) func() {
-	start := time.Now()
-	return func() {
-		fmt.Printf("%s took %v\n", what, time.Since(start))
-	}
-}
-
 func main() {
 
 	data := Load()
-	// log.Println(data)
 
 	dataFile, err_file_in := os.Create("integerdata.gob")
 	if err_file_in != nil {
@@ -59,63 +49,4 @@ func main() {
 		log.Fatal("encode error:", err)
 	}
 	dataFile.Close()
-}
-
-func writeFile(bytes []byte) int {
-	file, _ := os.Create("test.bin")
-	defer file.Close()
-
-	number_byte := writeNextBytes(file, bytes)
-	return number_byte
-}
-
-func readFile(number_byte int) []byte {
-	file, _ := os.Open("test.bin")
-	defer file.Close()
-
-	bytes_array := readNextBytes(file, number_byte)
-	return bytes_array
-}
-
-func readNextBytes(file *os.File, number int) []byte {
-	bytes := make([]byte, number)
-
-	_, err := file.Read(bytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return bytes
-}
-
-// func writeFile() {
-// 	file, err := os.Create("test.bin")
-// 	defer file.Close()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	r := Load()
-
-// 	for i := 0; i < 10; i++ {
-
-// 		s := r[i]
-// 		var bin_buf bytes.Buffer
-// 		binary.Write(&bin_buf, binary.BigEndian, s)
-// 		//b :=bin\_buf.Bytes()
-// 		//l := len(b)
-// 		//fmt.Println(l)
-// 		writeNextBytes(file, bin_buf.Bytes())
-// 	}
-// }
-
-func writeNextBytes(file *os.File, bytes []byte) int {
-
-	_, err := file.Write(bytes)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return len(bytes)
 }
